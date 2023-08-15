4b0c55355ea8dd42d2b90264f3c6f12845bbbb74
fgg100y <1522009317@qq.com>
2023-08-15 16:01:43 +0800

    Add class CustomDataset and using DataLoader

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,15 +1,28 @@
 import torch
-from torch.utils.data import Dataset, TensorDataset, DataLoader
+from torch.utils.data import TensorDataset, DataLoader
 from torch.utils.data.dataset import random_split
 from torch.utils.tensorboard import SummaryWriter
 from linear_regression_in_numpy import gen_data
+
+#  from src.data.preparation import CustomDataset
+
 import matplotlib.pyplot as plt
+
 plt.style.use("fivethirtyeight")
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 x_train, y_train, x_val, y_val = gen_data()
 
 # Our data was in Numpy arrays, but we need to transform them
-# into PyTorch's Tensors and then we send them to the chosen device
-x_train_tensor = torch.as_tensor(x_train).float().to(device)
-y_train_tensor = torch.as_tensor(y_train).float().to(device)
+# into PyTorch's Tensors
+x_train_tensor = torch.as_tensor(x_train).float()
+y_train_tensor = torch.as_tensor(y_train).float()
+
+train_data = CustomDataset(x_train_tensor, y_train_tensor)
+
+train_loader = DataLoader(
+    dataset=train_data,
+    batch_size=16,
+    shuffle=True,
+)
+#  next(iter(train_loader))
--- src/model_training.py
+++ src/model_training.py
@@ -1,9 +1,20 @@
+import numpy as np
 
-# Defines number of epochs
-n_epochs = 1000
+
+n_epochs = 1000  # Defines number of epochs
 
 losses = []
-
 for epoch in range(n_epochs):
-    loss = train_step_fn(x_train_tensor, y_train_tensor)
+    # inner loop (though mini-batches)
+    mini_batch_losses = []
+    for x_batch, y_batch in train_loader:
+        # need to send these mini-batch from CPU to GPU
+        x_batch = x_batch.to(device)
+        y_batch = y_batch.to(device)
+
+        # performs one train step and returns the loss
+        batch_loss = train_step_fn(x_batch, y_batch)
+        mini_batch_losses.append(batch_loss)
+
+    loss = np.mean(mini_batch_losses)
     losses.append(loss)
