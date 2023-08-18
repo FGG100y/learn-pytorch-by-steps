322d14f5a8d496326d3aa44ff6460ed806a2c737
fgg100y <1522009317@qq.com>
2023-08-18 11:00:55 +0800

    Add class TransformedTensorDataset

--- src/data/preparation/utils.py
+++ src/data/preparation/utils.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset, TensorDataset, DataLoader
 
 
+# for linear regression data
 class CustomDataset(Dataset):
     def __init__(self, x_tensor, y_tensor):
         self.x = x_tensor
@@ -8,6 +9,25 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, index):
         return (self.x[index], self.y[index])
+
+    def __len__(self):
+        return len(self.x)
+
+
+# for image classification data
+class TransformedTensorDataset(Dataset):
+    def __init__(self, x, y, transform=None):
+        self.x = x
+        self.y = y
+        self.transform = transform
+
+    def __getitem__(self, index):
+        x = self.x[index]
+
+        if self.transform:
+            x = self.transform(x)
+
+        return x, self.y[index]
 
     def __len__(self):
         return len(self.x)
