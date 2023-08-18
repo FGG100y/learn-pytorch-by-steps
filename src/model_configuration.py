e6737f2447361f8b8c44d32df49a8007c2896b15
fgg100y <1522009317@qq.com>
2023-08-18 16:11:28 +0800

    Add shallow model (logistic model) for image task

--- src/model_configuration.py
+++ src/model_configuration.py
@@ -9,15 +9,25 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 lr = 0.1
 torch.manual_seed(42)
 
+classification = True
+image_data = True
+
 model = nn.Sequential()
 
 if classification:
-    model.add_module("linear", nn.Linear(2, 1))
-    print(model.state_dict())
+    if image_data:
+        model.add_module("flatten", nn.Flatten())
+        model.add_module("output", nn.Linear(25, 1, bias=False))
+        model.add_module("sigmoid", nn.Sigmoid())
+        optimizer = optim.SGD(model.parameters(), lr=lr)
+        loss_fn = nn.BCELoss()
+    else:
+        model.add_module("linear", nn.Linear(2, 1))
+        print(model.state_dict())
 
-    optimizer = optim.SGD(model.parameters(), lr=lr)
+        optimizer = optim.SGD(model.parameters(), lr=lr)
 
-    loss_fn = nn.BCEWithLogitsLoss(reduction='mean')
+        loss_fn = nn.BCEWithLogitsLoss(reduction='mean')
 else:
     #  # create a model and send to device
     #  model = nn.Sequential(nn.Linear(1, 1)).to(device)
