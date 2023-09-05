da57a6be591378d4347a7677c57a0962ed3ea6a7
fgg100y <1522009317@qq.com>
2023-09-05 15:24:08 +0800

    Add RNNs

---
+++ src/model/rnn.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+
+
+class SquareModel(nn.Module):
+    def __init__(self, n_features, hidden_dim, n_outputs):
+        super(SquareModel, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.n_features = n_features
+        self.n_outputs = n_outputs
+        self.hidden = None
+        # simple rnn:
+        self.basic_rnn = nn.RNN(self.n_features,
+                                self.hidden_dim,
+                                batch_first=True)
+        # classifer
+        self.classifier = nn.Linear(self.hidden_dim, self.n_outputs)
+
+    def forward(self, X):
+        # X is batch-first (N, L, F) -- (num. of batch, length, feature)
+        # output is (N, L, H) -- (num. of batch, length, hidden)
+        # final hidden state is (1, N, H)
+        batch_first_output, self.hidden = self.basic_rnn(X)
+
+        # only last item in sequence (N, 1, H)
+        last_output = batch_first_output[:, -1]
+        # classifer will output (N, 1, n_outputs)
+        out = self.classifier(last_output)
+
+        # final output is (N, n_outputs)
+        return out.view(-1, self.n_outputs)
