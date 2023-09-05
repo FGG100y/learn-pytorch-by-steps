53740b690357a4153c55d11461dc88e0cc4aa310
fgg100y <1522009317@qq.com>
2023-09-05 17:29:06 +0800

    Add LSTM

--- src/model/rnn.py
+++ src/model/rnn.py
@@ -1,17 +1,20 @@
 import torch.nn as nn
 
 
-class SquareModelGRU(nn.Module):
+class SquareModelLSTM(nn.Module):
     def __init__(self, n_features, hidden_dim, n_outputs):
-        super(SquareModelGRU, self).__init__()
+        super(SquareModelLSTM, self).__init__()
         self.hidden_dim = hidden_dim
         self.n_features = n_features
         self.n_outputs = n_outputs
         self.hidden = None
-        # simple GRU:
-        self.basic_rnn = nn.GRU(self.n_features,
-                                self.hidden_dim,
-                                batch_first=True)
+        self.cell = None
+        # simple LSTM:
+        self.basic_rnn = nn.LSTM(
+            self.n_features,
+            self.hidden_dim,
+            batch_first=True,
+        )
         # classifer
         self.classifier = nn.Linear(self.hidden_dim, self.n_outputs)
 
@@ -19,7 +22,8 @@ class SquareModelGRU(nn.Module):
         # X is batch-first (N, L, F) -- (num. of batch, length, feature)
         # output is (N, L, H) -- (num. of batch, length, hidden)
         # final hidden state is (1, N, H)
-        batch_first_output, self.hidden = self.basic_rnn(X)
+        # final cell state is (1, N, H)
+        batch_first_output, (self.hidden, self.cell) = self.basic_rnn(X)
 
         # only last item in sequence (N, 1, H)
         last_output = batch_first_output[:, -1]
