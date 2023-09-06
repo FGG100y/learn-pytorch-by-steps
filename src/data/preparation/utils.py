b10155e56d2b8cb9124e5316ae816752c53b0777
fgg100y <1522009317@qq.com>
2023-09-06 10:35:42 +0800

    Update class CustomDataset()

--- src/data/preparation/utils.py
+++ src/data/preparation/utils.py
@@ -1,11 +1,11 @@
+import torch
 from torch.utils.data import Dataset, TensorDataset, DataLoader
 
 
-# for linear regression data
 class CustomDataset(Dataset):
-    def __init__(self, x_tensor, y_tensor):
-        self.x = x_tensor
-        self.y = y_tensor
+    def __init__(self, x, y):
+        self.x = [torch.as_tensor(s).float() for s in x]
+        self.y = torch.as_tensor(y).float().view(-1, 1)
 
     def __getitem__(self, index):
         return (self.x[index], self.y[index])
