6571486c6baa6c2903c9d5ccf748d454bcd8e062
fgg100y <1522009317@qq.com>
2023-09-19 16:39:23 +0800

    Add class MultiHeadAttention()

--- src/model/seq2seq_encoder_decoder.py
+++ src/model/seq2seq_encoder_decoder.py
@@ -180,3 +180,34 @@ class EncoderDecoderAttn(EncoderDecoder):
     def store_outputs(self, i, out):
         self.outputs[:, i:i+1, :] = out
         self.alphas[:, i:i+1, :] = self.decoder.attn.alphas
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_heads, d_model, input_dim=None, proj_values=True):
+        super().__init__()
+        self.linear_out = nn.Linear(n_heads * d_model, d_model)
+        self.attn_heads = nn.ModuleList([
+            Attention(d_model, input_dim=input_dim, proj_values=proj_values)
+            for _ in range(n_heads)
+            ])
+
+    def init_keys(self, key):
+        for attn in self.attn_heads:
+            attn.init_keys(key)
+
+    @property
+    def alphas(self):
+        # shape: (n_heads, N, 1, L (source))
+        return torch.stack([attn.alphas for attn in self.attn_heads], dim=0)
+
+    def output_function(self, contexts):
+        #  (N, 1, n_heads * D)
+        concatenated = torch.cat(contexts, axis=-1)
+        # Linear transform to go back to original dimension
+        out = self.linear_out(concatenated)     # (N, 1, D)
+        return out
+
+    def forward(self, query, mask=None):
+        contexts = [attn(query, mask=mask) for attn in self.attn_heads]
+        out = self.output_function(contexts)
+        return out
