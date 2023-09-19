788441f2293e8838c8d695dbfa59ae7a815f5e04
fgg100y <1522009317@qq.com>
2023-09-19 15:38:08 +0800

    Add class DecoderAttn()

--- src/model/seq2seq_encoder_decoder.py
+++ src/model/seq2seq_encoder_decoder.py
@@ -85,7 +85,7 @@ class Encoder(nn.Module):
         return rnn_out  # (N, L, F) shape, batch_first
 
 
-class Decoder(nn.Module):
+class DecoderAttn(nn.Module):
     def __init__(self, n_features, hidden_dim):
         super().__init__()
         self.hidden_dim = hidden_dim
@@ -96,7 +96,8 @@ class Decoder(nn.Module):
             self.hidden_dim,
             batch_first=True,
         )
-        self.regression = nn.Linear(self.hidden_dim, self.n_features)
+        self.attn = Attention(self.hidden_dim)
+        self.regression = nn.Linear(2*self.hidden_dim, self.n_features)
 
     # initializing decoder's hidden state using encoder's final hidden state
     def init_hidden(self, hidden_seq):
@@ -105,11 +106,14 @@ class Decoder(nn.Module):
         # but we need to make it: sequence-first (hidden state must always be)
         self.hidden = hidden_final.permute(1, 0, 2)  # (1, N, H)
 
-    def forward(self, X):  # X is (N, 1, F)
+    def forward(self, X, mask=None):  # X is (N, 1, F)
         # the recurrent layer both *uses* and *updates* the hidden state
         batch_first_output, self.hidden = self.basic_rnn(X, self.hidden)
-        last_output = batch_first_output[:, -1:]
-        out = self.regression(last_output)
+        query = batch_first_output[:, -1:]
+        # attention:
+        context = self.attn(query, mask=mask)
+        concatenated = torch.cat([context, query], axis=-1)
+        out = self.regression(concatenated)
 
         # (N, 1, F), the same shape as the input
         return out.view(-1, 1, self.n_features)
