807a94c4c0773c78c296598ce418141c11fb9fe3
fgg100y <1522009317@qq.com>
2023-09-19 16:10:04 +0800

    use Attention model

--- seq2seq_model_configuration.py
+++ seq2seq_model_configuration.py
@@ -1,16 +1,17 @@
 import copy
+
 import numpy as np
 import torch
-import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader, Dataset, random_split, TensorDataset
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset, TensorDataset, random_split
+
 from src.data_generation.square_sequences import generate_sequences
 
 
 def calc_alphas(ks, q):
-    """calculate attention scores
-    """
+    """calculate attention scores"""
     dims = q.size(-1)
     # (N, 1, H) x (N, H, L) -> (N, 1, L)
     products = torch.bmm(q, ks.permute(0, 2, 1))
@@ -21,9 +22,9 @@ def calc_alphas(ks, q):
 
 def attention_q_k_v_demo(teacher_forcing_proba=0.5, verbose=False):
     # a square (points with coordinates and directions as sequences)
-    full_seq = torch.tensor([
-        [-1, -1], [-1, 1], [1, 1], [1, -1]
-        ]).float().view(1, 4, 2)  # (N, L, F)
+    full_seq = (
+        torch.tensor([[-1, -1], [-1, 1], [1, 1], [1, -1]]).float().view(1, 4, 2)
+    )  # (N, L, F)
     source_seq = full_seq[:, :2]  # first two conners
     target_seq = full_seq[:, 2:]  # last two conners
 
@@ -42,10 +43,9 @@ def attention_q_k_v_demo(teacher_forcing_proba=0.5, verbose=False):
     torch.manual_seed(21)
     encoder = Encoder(n_features=2, hidden_dim=2)  # untrained model
     hidden_seq = encoder(source_seq)  # output shape (N, L, F)
-    hidden_final = hidden_seq[:, -1:]
-    if verbose == 2:
-        print("-*-encoder-*-")
-        print(f"the final hidden state:\n{hidden_final}\n")
+    # hidden states are both "Values" and "Keys":
+    keys = hidden_seq
+    values = hidden_seq
 
     decoder = Decoder(n_features=2, hidden_dim=2)
     # initial hidden state is encoder's final hidden state
@@ -53,26 +53,19 @@ def attention_q_k_v_demo(teacher_forcing_proba=0.5, verbose=False):
     # initial data point is the last element of source sequence
     inputs = source_seq[:, -1:]
 
-    if verbose == 2:
-        #  teacher_forcing_proba = 0.5
-        target_len = target_seq.shape[1]
-        for i in range(target_len):
-            print("-*-decoder-*-")
-            print(f"Hidden: {decoder.hidden}")
-            out = decoder(inputs)
-            print(f"Output: {out}\n")
-            # the predictions are next step's inputs
-            # NOTE the "teacher forcing":
-            # model trained using teacher forcing will minimize the loss given the
-            # correct inputs at every step of the target sequence. But this will
-            # never be the case ate testing time! And we must seek help from stats.
-            if torch.rand(1) <= teacher_forcing_proba:
-                inputs = target_seq[:, i:i+1]
-            else:
-                inputs = out
+    # the first "Query" is the decoder's hidden state (always sequence-first)
+    query = decoder.hidden.permute(1, 0, 2)  # N, 1, H
 
-    encdec = EncoderDecoder(encoder, decoder, input_len=2, target_len=2,
-                            teacher_forcing_proba=0.5)
+    alphas = calc_alphas(keys, query)
+    context_vec = torch.bmm(alphas, values)
+
+    concatenated = torch.cat([context_vec, query], axix=-1)
+    if verbose:
+        print(concatenated)
+
+    encdec = EncoderDecoder(
+        encoder, decoder, input_len=2, target_len=2, teacher_forcing_proba=0.5
+    )
     # model expects the full sequence
     # so it can randomly use the teacher forcing:
     encdec.train()
@@ -104,16 +97,18 @@ train_data = TensorDataset(full_train, target_train)
 test_data = TensorDataset(source_test, target_test)
 
 generator = torch.Generator()
-train_loader = DataLoader(train_data, batch_size=16, shuffle=True,
-                          generator=generator)  # ensure reproducibility
+train_loader = DataLoader(
+    train_data, batch_size=16, shuffle=True, generator=generator
+)  # ensure reproducibility
 test_loader = DataLoader(test_data, batch_size=16)
 
 # model configuration
-torch.manual_seed(23)
+torch.manual_seed(17)
 encoder = Encoder(n_features=2, hidden_dim=2)
-decoder = Decoder(n_features=2, hidden_dim=2)
-model = EncoderDecoder(encoder, decoder, input_len=2, target_len=2,
-                       teacher_forcing_proba=0.5)
+decoder = DecoderAttn(n_features=2, hidden_dim=2)
+model = EncoderDecoderAttn(
+    encoder, decoder, input_len=2, target_len=2, teacher_forcing_proba=0.5
+)
 loss = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.01)
 
